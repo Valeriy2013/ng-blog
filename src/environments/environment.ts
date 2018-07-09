19cbe6db79521350942f2f7c3ebe4424204aeefd
Valerii Sokolovskyi <vsokolovskyi@lohika.com>
2018-07-09 12:02:45 +0300

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBImDMOM94vsibT_mjY2JP9Qv8R1oCig7M",
+    authDomain: "ng-blog-7bfb8.firebaseapp.com",
+    databaseURL: "https://ng-blog-7bfb8.firebaseio.com",
+    projectId: "ng-blog-7bfb8",
+    storageBucket: "ng-blog-7bfb8.appspot.com",
+    messagingSenderId: "1043877852080"
+  }
 };
 
 /*
